6bb91519e0bae4f01ae5b0ca64355deb4a2ac22b
yuyud20 <yudhapasaribu20@gmail.com>
2023-10-10 13:23:22 +0700

    update users controller

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,21 @@ import express from "express";
 import { createUser, findUserById } from "../services/users.js";
 import { httpStatusMesage } from "../constants/httpStatusMessage.js";
 
-export const getUserById = async (request, response) => {
-  const { id } = request.params;
-  const user = await findUserById(id);
+export const postCreateUser = async (request, response) => {
+  const { userName, password, email, beratBadan, tinggiBadan, jenisKelamin } = request.body;
+  const user = await createBook(userName, password, email, beratBadan, tinggiBadan, jenisKelamin);
 
+  console.log({ body: request.body });
   response.json({
     data: user,
     message: httpStatusMesage[response.statusCode],
   });
 };
 
-export const postCreateUser = async (request, response) => {
-  const { userName, password, email, beratBadan, tinggiBadan, jenisKelamin } = request.body;
-  const user = await createBook(userName, password, email, beratBadan, tinggiBadan, jenisKelamin);
+export const getUserById = async (request, response) => {
+  const { id } = request.params;
+  const user = await findUserById(id);
 
-  console.log({ body: request.body });
   response.json({
     data: user,
     message: httpStatusMesage[response.statusCode],
